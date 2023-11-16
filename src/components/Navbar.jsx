4099591a0d38a2cc56508debbe0900c97c0b091e
Ayobami <tnenas07@gmail.com>
2023-11-16 17:49:19 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <header className="header">
+      <NavLink
+        to="/"
+        className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md "
+      >
+        <p className="blue-gradient_text">BA</p>
+      </NavLink>
+
+      <nav className="flex text-lg gap-7 font-medium">
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500' : 'text-black'
+          }
+        >
+          Home
+        </NavLink>
+
+        <NavLink
+          to="/about-me"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500' : 'text-black'
+          }
+        >
+          About me
+        </NavLink>
+
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500' : 'text-black'
+          }
+        >
+          Projects
+        </NavLink>
+
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? 'text-blue-500' : 'text-black'
+          }
+        >
+          Contact
+        </NavLink>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
